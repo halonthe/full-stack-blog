fa432e9d77aaaea8893f1df024c726c929e8671a
halonthe <halonthe@gmail.com>
2024-12-24 22:27:31 +0700

    feat: create navbar

---
+++ src/components/Image.jsx
@@ -0,0 +1,21 @@
+import { IKImage } from "imagekitio-react";
+import React from "react";
+
+const Image = ({ src, className, w, h, alt }) => {
+  return (
+    <div>
+      <IKImage
+        urlEndpoint={import.meta.env.VITE_IMAGEKIT_URL}
+        path={src}
+        loading="lazy"
+        className={className}
+        lqip={{ active: true, quality: 20 }}
+        width={w}
+        height={h}
+        alt={alt}
+      />
+    </div>
+  );
+};
+
+export default Image;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Image from "./Image";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* Logo */}
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <Image src="/haloblog/logo.png" alt="haloblog logo" w={32} h={32} />
+        <span>Haloblog.</span>
+      </div>
+      {/* Mobile Menu */}
+      <div className="md:hidden">
+        {/* Hamburger Icon */}
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "X" : "≡"}
+        </div>
+        {/* Mobile Menu Items */}
+        <div
+          className={`absolute top-16 w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login
+            </button>
+          </a>
+        </div>
+      </div>
+      {/* Desktop Menu */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
